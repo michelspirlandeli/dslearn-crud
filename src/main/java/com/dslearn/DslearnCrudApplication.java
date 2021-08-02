01e33fe07d0006c71cfa320292744eb4fdece56d
michelspirlandeli <michelspirlandeli10@gmail.com>
2021-08-02 18:45:31 -0300

    first commit

---
+++ src/main/java/com/dslearn/DslearnCrudApplication.java
@@ -0,0 +1,13 @@
+package com.dslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dslearn/DslearnCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
